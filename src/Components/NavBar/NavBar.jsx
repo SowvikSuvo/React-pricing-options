5b404f42b61976726666466b46a7a5cfe7173c08
SowvikSuvo <skarmoker44@gmail.com>
2025-09-28 16:53:36 +0600

    NavBar mapping

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "../../Link/Link";
+import { Menu, X } from "lucide-react";
 
 const navData = [
   { id: 1, name: "Home", path: "/" },
@@ -10,15 +11,25 @@ const navData = [
 ];
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  const links = navData.map((nav) => <Link key={nav.id} nav={nav}></Link>);
+
   return (
-    <nav>
-      <ul className="flex justify-center items-center ">
-        {navData.map((nav) => (
-          <Link nav={nav}></Link>
-        ))}
-      </ul>
+    <nav className="flex justify-between mx-10" onClick={() => setOpen(!open)}>
+      <span className="flex">
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <Menu className="md:hidden"></Menu>
+        )}
+
+        <ul className="md:hidden">{links}</ul>
+
+        <h3 className="ml-4">My NavBar</h3>
+      </span>
+      <ul className="md:flex hidden  items-center ">{links}</ul>
+      <button>Sign in</button>
     </nav>
   );
 };
-
 export default NavBar;
